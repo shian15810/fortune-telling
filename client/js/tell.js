3d22108f53dd6f2eef5f29edf9905f90a52845b0
Chuah Chee Shian <shian15810@gmail.com>
2016-12-26 08:56:36 +0800

    fixed bold text on company name

--- client/js/tell.js
+++ client/js/tell.js
@@ -4,7 +4,7 @@ $(() => {
   const hasData = (name, data) => {
     $('#success').html('<div class="alert alert-success">');
     $('#success > .alert-success').html('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;').append('</button>');
-    $('#success > .alert-success').append(`</strong>${name}</strong>`);
+    $('#success > .alert-success').append(`<strong>${name}</strong>`);
     $('#success > .alert-success').append('很有可能成為');
     $('#success > .alert-success').append(`<strong>${data.market}</strong>`);
     $('#success > .alert-success').append('公司哦！');
